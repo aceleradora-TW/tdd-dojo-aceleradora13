b13e507648863e9d130c4e70f489deaea70e467d
Ghilherme Ghidorsi <ghidorsi@thoughtworks.com>
2018-04-13 16:00:36 -0300

    exercicio tdd

--- src/calculations.js
+++ src/calculations.js
@@ -1,3 +1,15 @@
-export const max = () => null;
-export const length = () => null;
-export const average = () => null;
+export const max = (arrayDeInteiro) => {
+  if (Array.isArray(arrayDeInteiro)  && arrayDeInteiro.length != 0) {
+    for (let index = 0; index < arrayDeInteiro.length; index++) {
+      if (typeof arrayDeInteiro[index] != 'number'){
+        return "Mensagem de erro.";
+        break;
+      }    
+    }
+    return Math.max(...arrayDeInteiro);
+  } 
+    return "Mensagem de erro.";
+};
+
+// usar propriedade da linguagem javascript para verificar se existir vetor 
+// E tambem se o vetor nao é vazio. 
--- src/calculations.spec.js
+++ src/calculations.spec.js
@@ -1,4 +1,26 @@
 import { expect } from 'chai';
+import { max } from './calculations';
 
 describe('calculations', () => {
+  describe('max', () => {
+    it('nao deve aceitar string', () => {
+      expect(max('12345')).to.be.eq('Mensagem de erro.');
+    });
+
+    it('deve aceitar um array sem retornar erro', () => {
+      expect(max([1, 2, 3])).to.not.be.eq('Mensagem de erro.');
+    });
+
+    it ('o array nao deve ser vazio', ()=> {
+      expect(max([])).to.be.eq('Mensagem de erro.');
+    }); 
+
+    it ('o array nao deve conter caracter', () =>{
+      expect(max([1,'w',2,'r'])).to.be.eq('Mensagem de erro.');
+    });
+    it('Deve retornar o maior', () => {
+        expect(max([1,2])).to.be.eq(2)
+    }
+  )
+  });
 });
